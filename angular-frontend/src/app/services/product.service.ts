8d76298c05b8f9128417e98e95c22497ad11e8e8
Orhun Ege Ulker <egeulker99@gmail.com>
2021-12-17 13:54:45 +0300

    Populated Service with request and made it unwrap the JSON into a JS class

--- angular-frontend/src/app/services/product.service.ts
+++ angular-frontend/src/app/services/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import {Product} from '../common/prdouct';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+private baseUrl = 'http://localhost:8080/api/products'
+///spring server url to access REST service
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList() : Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
+}
+
+interface GetResponse{
+  _embedded : {
+    products: Product[];
+  }
 }
